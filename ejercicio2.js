7fec1a9965528c39ff76c81368f25ee01146e832
jjrg77 <jjrgmadrid@gmail.com>
2020-12-16 02:16:35 +0100

    segunda subida. ejercicios JS

---
+++ ejercicio2.js
@@ -0,0 +1,28 @@
+/* Se crea una máquina para dejar pasar a un local sólo a las personas mayores de edad, siendo gratis la entrada para la primera persona que entre después de las 2 AM. */
+
+/* let free = false;
+const validacion = (hora) => {
+    let edad = prompt("¿Cuál es tu edad?");
+    if (edad >= 18) {
+        if (hora >= 2 && hora < 7 && free == false){
+            alert(`tienes ${edad}, puedes pasar gratis. Son las ${hora} HRS`);
+            free = true;
+        }else{
+            
+            alert(`Puedes pasar, tienes ${edad}. La entrada vale 5€. Son las ${hora} HRS`);
+        }
+    }else{
+        alert(`No puedes pasar, eres menor de edad`);
+    }
+}
+
+validacion(23);
+validacion(2);
+validacion(3); */
+
+/* En un curso hay 19 alumnos, se ha roto el sistema de registro de asistencias y durante los próximos 30 días no se podrá hacer dicho registro:
+    -Crear un mini-sistema que nos permita registrar a los alumnos que están presentes (P) y ausentes (A) en clase. 
+    -Pasados los 30 días mostrar la situación final de todos los alumnos (nº total de presentes y ausentes). 
+    -Se puede tener un máximo de 10% de ausencias por SVGFEComponentTransferElement, si se tienen más, se suspende la asignatura. */
+
+    
